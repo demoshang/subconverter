1bcd0121f834bac35e4af441e2b0cbe03475a9a8
feng <xinshangshangxin@gmail.com>
2024-09-19 19:36:50 +0800

    feat: rm subconverter UA

--- src/handler/webget.cpp
+++ src/handler/webget.cpp
@@ -31,7 +31,8 @@ std::mutex cache_rw_lock;
 RWLock cache_rw_lock;
 
 //std::string user_agent_str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
-static auto user_agent_str = "subconverter/" VERSION " cURL/" LIBCURL_VERSION;
+// static auto user_agent_str = "subconverter/" VERSION " cURL/" LIBCURL_VERSION;
+static auto user_agent_str = "";
 
 struct curl_progress_data
 {
@@ -178,8 +179,8 @@ static int curlGet(const FetchArgument &argument, FetchResult &result)
         if(!argument.request_headers->contains("User-Agent"))
             curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, user_agent_str);
     }
-    header_list = curl_slist_append(header_list, "SubConverter-Request: 1");
-    header_list = curl_slist_append(header_list, "SubConverter-Version: " VERSION);
+    // header_list = curl_slist_append(header_list, "SubConverter-Request: 1");
+    // header_list = curl_slist_append(header_list, "SubConverter-Version: " VERSION);
     if(header_list)
         curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, header_list);
 
